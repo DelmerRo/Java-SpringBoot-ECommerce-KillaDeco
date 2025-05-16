183b16b1c62dc0760cdb3133d75bf4ea120def95
Delmer Teodomiro Rodriguez Gutierrez <jindrg@gmail.com>
2025-05-16 01:28:18 -0300

    feat: Add CI workflow using GitHub Actions

---
+++ src/main/java/com/killadeco/killadeco/KilladecoApplication.java
@@ -0,0 +1,13 @@
+package com.killadeco.killadeco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KilladecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KilladecoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/killadeco/killadeco/KilladecoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.killadeco.killadeco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KilladecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
